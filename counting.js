28c22077fe3b4a4a2c24ecdcbe4924d73698d0cb
Karen Casey-Gover <karen.casey@hotmail.com>
2016-10-05 18:32:47 +0000

    Completes for loop

---
+++ counting.js
@@ -0,0 +1,26 @@
+function countLetters(uniqueChar) {
+  for ( var nextLtr of uniqueChar)  {
+    console.log(nextLtr);
+  }
+
+
+}
+
+countLetters("lighthouse in the house");
+
+
+//var noSpaces = char.split(" ").join("");
+/*var numEachLtr =
+
+
+var myObj = {
+    : ' ',
+    : ' '
+  }
+
+}
+console.log(`I'm counting ${this.allChar} ${this.numChar}.`);
+
+countLetters("some string");
+
+//countLetters needs to return an Object*/
